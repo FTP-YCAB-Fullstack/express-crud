3c7a137dd00fb37fa5a26a909d25fb489fd99eb6
Ferraridiaz <87638147+Ferraridiaz@users.noreply.github.com>
2021-09-13 15:29:16 +0700

    proses mengerjakan

--- app.js
+++ app.js
@@ -3,23 +3,59 @@
 const express =  require('express');
 const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
+
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true }));
 
-const student = ['nama, role'];
+const students = [
+    {
+        id: 1,
+        nama: 'ferrari diaz',
+        role: 'student'
+    },
+    {
+        id: 2,
+        nama: 'dimas maulana',
+        role: 'student'
+    },
+    {
+        id: 3,
+        nama: 'mulia',
+        role: 'student'
+    },
+    {
+        id: 4,
+        nama: 'yusril',
+        role: 'student'
+    },
+];
 
-app.get('/', (req,res) => {
-    res.status(200).send({
-        msg: student,
-    });
+const mentor = [
+    {
+        id: 1,
+        nama: 'eddy permana',
+        batch: 'fantastic falkor'
+    },
+    {
+        id: 2,
+        nama: 'afista pratama',
+        batch: 'fantastic falkor'
+    },
+    {
+        id: 3,
+        nama: 'luthfi',
+        batch: 'fantastic falkor'
+    },
+];
+
+app.get('/students', (req,res) => {
+    res.send({
+        students: students
+    })
 });
 
-// res.json({
-//     'success'
-// });
+
 
 
 app.listen(3000, () => console.log('server running'));
