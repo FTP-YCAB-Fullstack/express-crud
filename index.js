aca4b9bbb2045b12e29f53ae673ed0cf20c0095c
renitanathania <renitans16@gmail.com>
2021-09-14 11:26:16 +0700

    Memasukan node_modules ke .gitignore

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const express = require('express');
 
 const index = express();
 
-index.get('/', (req, res) => {
-    res.send('server running');
-});
+// index.get('/', (req, res) => {
+//     res.send('server running');
+// });
+// // No 1
+// index.get("/crud", (req, res) => {
+//     res.send("Students, Mentors, Course")
+// })
 
-index.get("/crud", (req, res) => {
-    res.send("Students, Mentors, Course")
-})
+//Percobaan
+// index.get("/crud1", (req, res) => {
+//     res.send('<h1>apa</h1>');
+// });
+
+// index.get('/students/:name', (req, res) => {
+//     res.send(`Hallo $(req.params.name)`);
+// });
+
 
 index.listen(5000, () => console.log('server running'));
