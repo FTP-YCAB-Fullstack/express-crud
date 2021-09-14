2cc875b6b051d12e61a7798dc16863e23539c0fe
Ferraridiaz <87638147+Ferraridiaz@users.noreply.github.com>
2021-09-14 10:29:51 +0700

    selesai mengerjakan

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({extended: true }));
 
-const students = [
+const studentsList = [
     {
         id: 1,
         nama: 'ferrari diaz',
@@ -31,7 +31,7 @@ const students = [
     },
 ];
 
-const mentor = [
+const mentorList = [
     {
         id: 1,
         nama: 'eddy permana',
@@ -49,13 +49,57 @@ const mentor = [
     },
 ];
 
+const course = [{
+    nama: 'expressJS',
+    tingkatKesulitan: 'expert'
+}];
+
+
 app.get('/students', (req,res) => {
     res.send({
-        students: students
+        students: studentsList
     })
 });
 
+app.post('/students', (req,res) => {
+    const {id, nama, role} = req.body
+    studentsList.push({id, nama, role})
+    res.send({
+        msg: 'create student',
+        student: (id, nama, role)
+    });
+});
 
+
+app.get('/mentors', (req,res) => {
+    res.send({
+        mentors: mentorList
+    })
+});
+
+app.post('/mentors', (req,res) => {
+    const {id, nama, batch} = req.body
+    mentorList.push({id, nama, batch})
+    res.send({
+        msg: 'create mentor',
+        mentor: (id, nama, batch)
+    });
+});
+
+app.get('/course', (req,res) => {
+    res.send({
+        courses: course
+    })
+});
+
+app.post('/course', (req,res) => {
+    const {nama, tingkatKesulitan} = req.body
+    course.push({nama, tingkatKesulitan})
+    res.send({
+        msg: 'create course',
+        courses: (nama, tingkatKesulitan)
+    });
+});
 
 
 app.listen(3000, () => console.log('server running'));
