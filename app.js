6557c132fa09a1d71a83dfff09c712fa89100c94
NeoGlanding <neoglanding@protonmail.com>
2021-09-14 11:18:05 +0700

    perbaikan

--- app.js
+++ app.js
@@ -93,7 +93,13 @@ app.get('/courses/:id', (req, res) => {
 
 
 app.post('/mentors', (req, res) => {
-    mentors.push({...req.body, id: mentors.length + 1});
+    let {name, batch} = req.body;
+    let newObj = {
+        id: mentors.length + 1,
+        name,
+        batch
+    }
+    mentors.push(newObj);
     res.status(201).json({
         msg: 'success',
         mentors
@@ -101,7 +107,13 @@ app.post('/mentors', (req, res) => {
 });
 
 app.post('/courses', (req, res) => {
-    courses.push({...req.body, id: courses.length + 1});
+    let {nama, kesulitan} = req.body;
+    let newObj = {
+        id: courses.length + 1,
+        nama,
+        kesulitan
+    }
+    courses.push(newObj);
     res.status(201).json({
         msg: 'success',
         courses
@@ -109,7 +121,13 @@ app.post('/courses', (req, res) => {
 });
 
 app.post('/students', (req, res) => {
-    students.push({...req.body, id: students.length + 1});
+    let {name, role} = req.body;
+    let newObj = {
+        id: students.length + 1,
+        name,
+        role
+    }
+    students.push(newObj);
     res.status(201).json({
         msg: 'success',
         students
@@ -118,80 +136,113 @@ app.post('/students', (req, res) => {
 
 app.patch('/mentors/:id', (req, res) => {
     const index = req.params.id - 1;
-    mentors[index] = {...mentors[index], ...req.body}
-    res.status(201).json({
+    let {name, batch} = req.body;
+    const newEdited = {
+        name,
+        batch
+    }
+    for (let undef in newEdited) {
+        if (!newEdited[undef]) delete newEdited[undef]
+    }
+    mentors[index] = {...mentors[index], ...newEdited}
+    res.status(200).json({
         status: 'success',
-        mentors
+        data: mentors[index]
     })
 })
 
 app.patch('/students/:id', (req, res) => {
     const index = req.params.id - 1;
-    students[index] = {...students[index], ...req.body}
-    res.status(201).json({
+    let {name, role} = req.body;
+    const newEdited = {
+        name,
+        role
+    }
+    for (let undef in newEdited) {
+        if (!newEdited[undef]) delete newEdited[undef]
+    }
+    students[index] = {...students[index], ...newEdited}
+    res.status(200).json({
         status: 'success',
-        students
+        data: students[index]
     })
 })
 
 app.patch('/courses/:id', (req, res) => {
     const index = req.params.id - 1;
-    courses[index] = {...courses[index], ...req.body}
-    res.status(201).json({
+    let {nama, kesulitan} = req.body;
+    const newEdited = {
+        nama,
+        kesulitan
+    }
+    for (let undef in newEdited) {
+        if (!newEdited[undef]) delete newEdited[undef]
+    }
+    courses[index] = {...courses[index], ...newEdited}
+    res.status(200).json({
         status: 'success',
-        courses
+        data: courses[index]
     })
 });
 
 app.put('/students/:id', (req, res) => {
     const index = req.params.id - 1;
-    students[index] = {...req.body};
-    res.status(201).json({
+    let {name, role} = req.body;
+    const newObj = {
+        id: req.params.id * 1,
+        name,
+        role
+    }
+    students[index] = {...newObj};
+    res.status(200).json({
         status: 'success',
-        students
+        data: students[index]
     })
 })
 
 app.put('/mentors/:id', (req, res) => {
     const index = req.params.id - 1;
-    mentors[index] = {...req.body};
+    let {name, batch} = req.body;
+    const newObj = {
+        id: req.params.id * 1,
+        name,
+        batch
+    }
+    mentors[index] = {...newObj};
     res.status(201).json({
         status: 'success',
-        mentors
+        data: mentors[index]
     })
 });
 
 app.put('/courses/:id', (req, res) => {
     const index = req.params.id - 1;
-    courses[index] = {...req.body};
+    let {nama, kesulitan} = req.body;
+    const newObj = {
+        id: req.params.id * 1,
+        nama,
+        kesulitan
+    }
+    courses[index] = {...newObj};
     res.status(201).json({
         status: 'success',
-        courses
+        data: courses[index]
     })
 });
 
 app.delete('/students/:id', (req, res) => {
     students = students.filter(el => el.id !== +req.params.id);
-    res.status(200).json({
-        status: 'success',
-        students
-    })
+    res.status(204).json({})
 })
 
 app.delete('/mentors/:id', (req, res) => {
     mentors = mentors.filter(el => el.id !== +req.params.id);
-    res.status(200).json({
-        status: 'success',
-        mentors
-    })
+    res.status(200).json({})
 })
 
 app.delete('/courses/:id', (req, res) => {
     courses = courses.filter(el => el.id !== +req.params.id);
-    res.status(200).json({
-        status: 'success',
-        courses
-    })
+    res.status(204).json({})
 })
 
 
