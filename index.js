797047006b52666f6dd5979ef7eff42c264e9b22
GanXx8 <ganda.imasxx@gmail.com>
2021-09-14 10:35:47 +0700

    add informasi API

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
 'use strict'
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+
+const student = [
+    {
+        id = 1,
+        nama = 'Ganda imas',
+        status = 'student'
+    },
+    {
+        id = 2,
+        nama = 'Alim Makruf',
+        status = 'student'
+    },
+    {
+        id = 3,
+        nama = 'Kelvin',
+        status = 'student'
+    }
+]
+
+const mentor = [
+    {
+        id = 1, 
+        nama = 'Eddy Permana',
+        bacth  = 'Fantastic Falkor'
+    },
+    {
+        id = 2, 
+        nama = 'Afista Pratama',
+        bacth  = 'Fantastic Falkor'
+    },
+    {
+        id = 3, 
+        nama = 'Lutfi Fitra',
+        bacth  = 'Fantastic Falkor'
+    }
+]
+
+const course = [
+    {
+        id = 1,
+        namacourse = 'java script',
+        kesulitan = 'tinggi'
+    },
+    {
+        id = 2,
+        namacourse = 'frameworks Css',
+        kesulitan = 'medium'
+    },
+    {
+        id = 3, 
+        namacourse = 'Html',
+        kesulitan = 'medium'
+    }
+]
 
 app.get('/', (req, res) => {
     res.send('server is running')
 });
 
-app.get('/Api/:name', (req, res) => {
+app.get('/Api', (req, res) => {
     res.send('API will Here!')
 });
 
