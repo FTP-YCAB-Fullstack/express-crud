9038a629c4b5385ff54182ceef689d5c59bbae13
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2021-09-14 11:17:01 +0700

    hapus buku

--- app.js
+++ app.js
@@ -11,47 +11,6 @@ app.get("/",(req,res) => {
     res.send("INI HALAMAN UTAMA")
 })
 
-const books = [
-    {
-        id : 1,
-        name : "Belajar HTML"
-    },
-    {
-        id : 2,
-        name : "Belajar CSS"
-    },
-    {
-        id : 3,
-        name : "Belajar Javascript"
-    },
-]
-
-app.get("/", (req,res) => {
-    res.send("server running (route)")
-})
-
-app.post("/books/create",(req,res) => {
-    let payload = {
-        id : +req.body.id,
-        name : req.body.namaBuku
-    }
-
-    books.push(payload)
-    
-    res.send(books)
-})
-
-app.get("/books",(req,res) => {
-    res.status(200).json(books)
-})
-
-app.get("/books/:id",(req,res) => {
-    res.status(200).json(books.filter(book => book.id === +req.params.id))
-})
-
-
-
-
 let students = [
     {
         id : 1,
